f3129d95b10e587ebde334c618982c1799cf0554
Admin <admin@FerfromHell.local>
2018-09-10 17:43:50 -0700

    enum 2

--- js/enums.js
+++ js/enums.js
@@ -5,3 +5,19 @@ var Direction;
     Direction[Direction["Left"] = 2] = "Left";
     Direction[Direction["Right"] = 3] = "Right";
 })(Direction || (Direction = {}));
+;
+var Status;
+(function (Status) {
+    Status[Status["Ready"] = 0] = "Ready";
+    Status[Status["Waiting"] = 1] = "Waiting";
+})(Status || (Status = {}));
+;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+;
+var statusE = Status.Ready;
+//status = Color.Green;  // Error
--- ts/enums.ts
+++ ts/enums.ts
@@ -3,4 +3,10 @@ enum Direction {
     Down,
     Left,
     Right,
-}
+};
+
+enum Status { Ready, Waiting };
+enum Color { Red, Blue, Green };
+
+let statusE = Status.Ready;
+//status = Color.Green;  // Error
